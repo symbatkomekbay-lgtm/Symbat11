cd17d9931c13689d2cf0789de776ee557b213475
symbatkomekbay-lgtm <symbatkomekbay@gmail.com>
2026-09-19 00:41:53 +0500

    Update basic_functions.py

--- Practice-03/functions/basic_functions.py
+++ Practice-03/functions/basic_functions.py
@@ -1,1 +1,22 @@
+#1
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+#2
+def my_function():
+  print("Hello from a function")
+
+my_function()
+#3
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
+#4
+
 
