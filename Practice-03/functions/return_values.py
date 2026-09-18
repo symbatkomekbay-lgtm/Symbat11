e717547df20b54570f805807b065ab8c797ceff6
symbatkomekbay-lgtm <symbatkomekbay@gmail.com>
2026-09-19 00:45:04 +0500

    Update return_values.py

--- Practice-03/functions/return_values.py
+++ Practice-03/functions/return_values.py
@@ -1,1 +1,16 @@
+#1
+def get_greeting():
+  return "Hello from a function"
 
+message = get_greeting()
+print(message)
+
+#2
+def get_greeting():
+  return "Hello from a function"
+
+print(get_greeting())
+
+#3
+def my_function():
+  pass
