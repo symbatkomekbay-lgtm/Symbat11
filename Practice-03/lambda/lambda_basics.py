5cd434ba4742cb7a61ca4ffd97d68b7d8a05aecf
symbatkomekbay-lgtm <symbatkomekbay@gmail.com>
2026-09-19 00:47:58 +0500

    Update lambda_basics.py

--- Practice-03/lambda/lambda_basics.py
+++ Practice-03/lambda/lambda_basics.py
@@ -1,1 +1,14 @@
+#1
+x = lambda a : a + 10
+print(x(5))
 
+#2
+x = lambda a, b : a * b
+print(x(5, 6))
+#3
+x = lambda a, b, c : a + b + c
+print(x(5, 6, 2))
+#4
+def myfunc(n):
+  return lambda a : a * n
+  
