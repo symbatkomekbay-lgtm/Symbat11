285a9ebc64635fbff73e1b74ceaa4c4448bdd565
symbatkomekbay-lgtm <symbatkomekbay@gmail.com>
2026-09-19 00:55:02 +0500

    Update class_methods.py

--- Practice-03/classes/class_methods.py
+++ Practice-03/classes/class_methods.py
@@ -8,3 +8,15 @@ p1 = Person("Emil", 36)
 
 print(p1.name)
 print(p1.age)
+
+#2
+class Calculator:
+  def add(self, a, b):
+    return a + b
+
+  def multiply(self, a, b):
+    return a * b
+
+calc = Calculator()
+print(calc.add(5, 3))
+print(calc.multiply(4, 7))
