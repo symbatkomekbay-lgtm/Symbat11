515f9b8db4cbf162a31c7295a4d4acad415b20a2
symbatkomekbay-lgtm <symbatkomekbay@gmail.com>
2026-09-19 00:44:07 +0500

    Update function_arguments.py

--- Practice-03/functions/function_arguments.py
+++ Practice-03/functions/function_arguments.py
@@ -1,1 +1,26 @@
+#1
+def my_function(fname):
+  print(fname + " Refsnes")
 
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+#2
+def my_function(name): # name is a parameter
+  print("Hello", name)
+
+my_function("Emil") # "Emil" is an argument
+#3
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
+#4
+def my_function(name = "friend"):
+  print("Hello", name)
+
+my_function("Emil")
+my_function("Tobias")
+my_function()
+my_function("Linus")
