37261a9b76c906cdbb72de91fa6e1016849372d9
symbatkomekbay-lgtm <symbatkomekbay@gmail.com>
2026-09-19 00:59:08 +0500

    Update multiple_inheritance.py

--- Practice-03/inheritance/multiple_inheritance.py
+++ Practice-03/inheritance/multiple_inheritance.py
@@ -1,1 +1,13 @@
+class Person:
+  def __init__(self, name, age, city, country):
+    self.name = name
+    self.age = age
+    self.city = city
+    self.country = country
 
+p1 = Person("Linus", 30, "Oslo", "Norway")
+
+print(p1.name)
+print(p1.age)
+print(p1.city)
+print(p1.country)
