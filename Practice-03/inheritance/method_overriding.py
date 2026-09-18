a044e474148dbba7614bbb9032cfb2152e8bca25
symbatkomekbay-lgtm <symbatkomekbay@gmail.com>
2026-09-19 01:04:28 +0500

    Update method_overriding.py

--- Practice-03/inheritance/method_overriding.py
+++ Practice-03/inheritance/method_overriding.py
@@ -1,1 +1,17 @@
+#parent class
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
 
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+#child class
+class Student(Person):
+  pass
